f713d767d3cc6de9933c25beb31e340bbe3aa3ea
christian cooney <christiancooney@hotmail.com>
2022-11-16 16:35:19 +0000

    starters

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Dark & Small Basement Flat London',
+  address: 'London N1 1DT',
+  description: 'An affordable flat',
+  price_per_night: 45,
+  number_of_guests: 2
+)
+Flat.create!(
+  name: 'Palatial Mansion London',
+  address: 'Kensington London K1 1DT',
+  description: 'Live like the rich',
+  price_per_night: 775,
+  number_of_guests: 18
+)
+Flat.create!(
+  name: 'Houseboat London',
+  address: 'Union Canal London E9 1DT',
+  description: 'Unique house boat',
+  price_per_night: 75,
+  number_of_guests: 4
+)
